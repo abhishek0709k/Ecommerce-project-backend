9d9cd5dad90339759b3d158d611f65164d70b75b
abhishek0709k <abhishekkumarsingh844141@gmail.com>
2025-04-12 18:50:32 +0530

    add frontned url for cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: ["https://67fa67a9b8dd3f44649683ef--ecommerce-frontend-project.netlify.app", "http://localhost:5173"],
     credentials: true
 };
 
@@ -19,7 +19,7 @@ connectDB();
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/auth", authRouter);
 app.use("/cart", cartRouter);
